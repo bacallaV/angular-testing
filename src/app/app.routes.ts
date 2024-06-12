68dade44d3e631ecb9db10ca7713e942e58089af
bacallaV <valdohalo3@hotmail.com>
2024-06-12 17:22:06 -0600

    feat: :sparkles: base structure of components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+/* Components */
+import { HomeComponent } from './pages/home/home.component';
+import { PicoPreviewComponent } from './pages/pico-preview/pico-preview.component';
+import { ProductsComponent } from './pages/products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'pico-preview',
+    component: PicoPreviewComponent,
+  },
+  {
+    path: 'products',
+    component: ProductsComponent,
+  },
+];
