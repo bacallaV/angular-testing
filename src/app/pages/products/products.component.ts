424d5f1200d2cb5a9e01970e3c2202b750098e1a
bacallaV <valdohalo3@hotmail.com>
2024-06-12 19:00:39 -0600

    feat: :sparkles: add base products page

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { ProductsService } from '../../services/products.service';
+
+import { Product } from '../../interfaces/product.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  public products: Product[] = [];
 
+  constructor(private readonly productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.productsService.getAll(10, 0)
+      .subscribe(products => this.products = products);
+  }
 }
---
+++ src/app/services/products.service.ts
@@ -0,0 +1,76 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
+import { retry, catchError, map } from 'rxjs/operators';
+import { Observable, throwError, zip } from 'rxjs';
+
+import { Product, CreateProductDTO, UpdateProductDTO } from './../interfaces/product.interface';
+import { environment } from './../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  private readonly apiUrl = `${environment.API_URL}/products`;
+
+  constructor(
+    private readonly httpClient: HttpClient,
+  ) { }
+
+  public getAll(limit?: number, offset?: number): Observable<Product[]> {
+    let params = new HttpParams();
+
+    if (limit != null && limit != undefined && offset != null && offset != undefined) {
+      params = params.append('limit', limit);
+      params = params.append('offset', limit);
+    }
+
+    return this.httpClient.get<Product[]>(this.apiUrl, { params })
+    .pipe(
+      retry(3),
+      map(products => products.map(item => {
+        return {
+          ...item,
+          taxes: .19 * item.price
+        };
+      }))
+    );
+  }
+
+  public fetchReadAndUpdate(id: string, dto: UpdateProductDTO): Observable<[Product, Product]> {
+    return zip(
+      this.getOne(id),
+      this.update(id, dto)
+    );
+  }
+
+  public getOne(id: string): Observable<Product> {
+    return this.httpClient.get<Product>(`${this.apiUrl}/${id}`)
+    .pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === HttpStatusCode.Conflict) {
+          return throwError('Algo esta fallando en el server');
+        }
+        if (error.status === HttpStatusCode.NotFound) {
+          return throwError('El producto no existe');
+        }
+        if (error.status === HttpStatusCode.Unauthorized) {
+          return throwError('No estas permitido');
+        }
+        return throwError('Ups algo salio mal');
+      })
+    );
+  }
+
+  public create(dto: CreateProductDTO): Observable<Product> {
+    return this.httpClient.post<Product>(this.apiUrl, dto);
+  }
+
+  public update(id: string, dto: UpdateProductDTO): Observable<Product> {
+    return this.httpClient.put<Product>(`${this.apiUrl}/${id}`, dto);
+  }
+
+  public delete(id: string): Observable<boolean> {
+    return this.httpClient.delete<boolean>(`${this.apiUrl}/${id}`);
+  }
+}
