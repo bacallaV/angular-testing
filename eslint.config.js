0f9d0ce0d89808558872ac0d42ca5c52fdff651f
bacallaV <valdohalo3@hotmail.com>
2024-06-12 18:58:54 -0600

    chore: :bug: fix eslint config issues

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
-  
+
 module.exports = tseslint.config(
   {
     files: ["**/*.ts"],
@@ -14,6 +14,102 @@ module.exports = tseslint.config(
     ],
     processor: angular.processInlineTemplates,
     rules: {
+      /* Custom ESLint rules for TypeScript */
+      "no-console": [
+        "error",
+        {
+          "allow": ["error"]
+        }
+      ],
+      "quotes": [
+        "error",
+        "single"
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/array-type": [
+        "error",
+        {
+          "default": "array"
+        }
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "argsIgnorePattern": "_",
+          "args": "all"
+        }
+      ],
+      "@typescript-eslint/explicit-function-return-type": [
+        "error",
+        {
+          "allowedNames": [
+            "canActivate",
+            "intercept",
+            "transform"
+          ]
+        }
+      ],
+      "@typescript-eslint/explicit-member-accessibility": [
+        "error",
+        {
+          "accessibility": "explicit",
+          "overrides": {
+            "accessors": "explicit",
+            "constructors": "off",
+            "methods": "explicit",
+            "properties": "explicit",
+            "parameterProperties": "explicit"
+          },
+          "ignoredMethodNames": [
+            "canActivate",
+            "intercept",
+            "transform",
+            "ngOnInit",
+            "ngOnDestroy",
+            "ngOnChanges",
+            "ngAfterViewInit",
+            "ngAfterViewChecked"
+          ]
+        }
+      ],
+      "@typescript-eslint/no-non-null-assertion": "warn",
+      "@typescript-eslint/no-inferrable-types": "off",
+      "space-before-blocks": "error",
+      "semi": [
+        "error"
+      ],
+      "no-extra-semi": "error",
+      "semi-style": [
+        "error",
+        "last"
+      ],
+      "semi-spacing": [
+        "error",
+        {
+          "before": false,
+          "after": true
+        }
+      ],
+      "@typescript-eslint/no-empty-function": [
+        "error",
+        {
+          "allow": [
+            "constructors",
+            "arrowFunctions"
+          ]
+        }
+      ],
+      "@typescript-eslint/parameter-properties": [
+        "error",
+        {
+          "allow": [
+            "public readonly",
+            "protected readonly",
+            "private readonly"
+          ]
+        }
+      ],
+      /* Angular ESLint rules */
       "@angular-eslint/directive-selector": [
         "error",
         {
