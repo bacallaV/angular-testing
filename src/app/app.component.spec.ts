95dc43ecb42f889614bf13023813b1f8e11f159b
bacallaV <valdohalo3@hotmail.com>
2024-06-13 06:44:34 -0600

    style: :rotating_light: lint code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-testing' title`, () => {
+  it('should have the "angular-testing" title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-testing');
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-testing';
+  public readonly title = 'angular-testing';
 }
