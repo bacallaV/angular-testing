5a2b3a69f09d2c9c6b19d060b791f5b4a4091c92
bacallaV <valdohalo3@hotmail.com>
2024-06-13 06:45:17 -0600

    test: :white_check_mark: add products service basic test

--- src/app/services/products.service.spec.ts
+++ src/app/services/products.service.spec.ts
@@ -1,16 +1,76 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 import { ProductsService } from './products.service';
 
-describe('ProductsService', () => {
+import { Product } from '../interfaces/product.interface';
+import { environment } from '../../environments/environment';
+import { firstValueFrom } from 'rxjs';
+
+fdescribe('ProductsService', () => {
   let service: ProductsService;
+  let httpTesting: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
+    });
     service = TestBed.inject(ProductsService);
+    httpTesting = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getAll()', () => {
+    it('should return a product list', async () => {
+      // Arrange
+      const mockData: Product[] = [{
+        id: '123',
+        title: 'title',
+        price: 123,
+        description: 'description',
+        category: {
+          id: 123,
+          name: 'name',
+        },
+        images: ['img1', 'img2'],
+        taxes: 123 * .19,
+      }];
+      const product$ = service.getAll();
+      const productPromise = firstValueFrom(product$);
+
+      const req = httpTesting
+        .expectOne(`${environment.API_URL}/products`, 'Request to get all products');
+
+      // Assert: se pueden hacer varias aserciones del método.
+      expect(req.request.method).toBe('GET');
+
+      // Las últimas dos líneas de código pueden ser resumidas así
+      // const req = httpTesting
+      //   .expectOne({
+      //     method: 'GET',
+      //     url: `${environment.API_URL}/products`
+      //   }, 'Request to get all products');
+
+      // Esto provoca que la consulta sea completada, devolviendo el resultado.
+      req.flush(mockData);
+
+      const products = await productPromise;
+      expect(products).toEqual(mockData);
+      expect(products).toHaveSize(mockData.length);
+
+      httpTesting.verify();
+    });
+  });
+
+  afterEach(() => {
+    // Verify that none of the tests make any extra HTTP requests.
+    TestBed.inject(HttpTestingController).verify();
   });
 });
