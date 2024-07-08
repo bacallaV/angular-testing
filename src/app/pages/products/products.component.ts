dd7a6c683e434008e1e180bab8c80f04abc4c0b0
bacallaV <valdohalo3@hotmail.com>
2024-07-08 11:46:18 -0600

    feat: :sparkles: product component with testing

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -5,21 +5,27 @@ import { ProductsService } from '../../services/products/products.service';
 
 import { Product } from '../../interfaces/product.interface';
 import { CommonModule } from '@angular/common';
+import { ProductComponent } from '../../components/product/product.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, ProductComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent implements OnInit {
   public products: Product[] = [];
+  public selectedProduct: Product | undefined;
 
   constructor(private readonly productsService: ProductsService) {}
 
   ngOnInit(): void {
     this.productsService.getAll(10, 0)
       .subscribe(products => this.products = products);
+  }
+
+  public onSelectedProduct(product: Product): void {
+    this.selectedProduct = product;
   }
 }
