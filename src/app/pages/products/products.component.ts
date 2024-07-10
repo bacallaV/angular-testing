52e2f39df5f74089def44579508a031c79ed1b16
bacallaV <valdohalo3@hotmail.com>
2024-07-10 13:36:19 -0600

    feat: :triangular_flag_on_post: pagination for products-component

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -15,14 +15,51 @@ import { ProductComponent } from '../../components/product/product.component';
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent implements OnInit {
-  public products: Product[] = [];
+
+  public products: Product[];
   public selectedProduct: Product | undefined;
 
-  constructor(private readonly productsService: ProductsService) {}
+  private readonly pagination = {
+    limit: 10,
+    offset: 0,
+  };
+  public status: 'loading' | 'success' | 'error' | 'no-more-products';
+
+  constructor(private readonly productsService: ProductsService) {
+    this.products = [];
+    this.status = 'success';
+  }
 
   ngOnInit(): void {
-    this.productsService.getAll(10, 0)
-      .subscribe(products => this.products = products);
+    this.pagination.offset -= this.pagination.limit;
+    this.getPaginatedProducts();
+  }
+
+  public getPaginatedProducts(): void {
+    if(this.status === 'no-more-products') return;
+
+    this.status = 'loading';
+
+    this.productsService.getAll(
+      this.pagination.limit,
+      this.pagination.offset + this.pagination.limit
+    ).subscribe({
+      next: (products) => {
+        if(products.length === 0) {
+          this.status = 'no-more-products';
+          return;
+        }
+
+        this.status = 'success';
+        this.products.push(...products);
+        this.pagination.offset += this.pagination.limit;
+      },
+      error: () => {
+        setTimeout(() => {
+          this.status = 'error';
+        }, 3000);
+      },
+    });
   }
 
   public onSelectedProduct(product: Product): void {
