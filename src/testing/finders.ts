f67f4dbd4d8db19849f78430a124acab527f090c
bacallaV <valdohalo3@hotmail.com>
2024-07-17 13:29:07 -0600

    test: :technologist: add finder not found error

--- src/testing/finders.ts
+++ src/testing/finders.ts
@@ -1,19 +1,35 @@
-import { DebugElement, Type } from '@angular/core';
+import { DebugElement, Predicate, Type } from '@angular/core';
 import { ComponentFixture } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
+function query<T>(fixture: ComponentFixture<T>, predicate: Predicate<DebugElement>): DebugElement {
+  const debugElement = fixture.debugElement.query(predicate);
+
+  if(!debugElement) throw new Error(`${predicate} not found`);
+
+  return debugElement;
+}
+
+function queryAll<T>(fixture: ComponentFixture<T>, predicate: Predicate<DebugElement>): DebugElement[] {
+  const debugElement = fixture.debugElement.queryAll(predicate);
+
+  if(!debugElement) throw new Error(`${predicate} not found`);
+
+  return debugElement;
+}
+
 export function queryByCSS<T>(fixture: ComponentFixture<T>, selector: string): DebugElement {
-  return fixture.debugElement.query(By.css(selector));
+  return query(fixture, By.css(selector));
 }
 
 export function queryAllByCSS<T>(fixture: ComponentFixture<T>, selector: string): DebugElement[] {
-  return fixture.debugElement.queryAll(By.css(selector));
+  return queryAll(fixture, By.css(selector));
 }
 
 export function queryByDirective<T, D>(fixture: ComponentFixture<T>, directive: Type<D>): DebugElement {
-  return fixture.debugElement.query(By.directive(directive));
+  return query(fixture, By.directive(directive));
 }
 
 export function queryAllByDirective<T, D>(fixture: ComponentFixture<T>, directive: Type<D>): DebugElement[] {
-  return fixture.debugElement.queryAll(By.directive(directive));
+  return queryAll(fixture, By.directive(directive));
 }
